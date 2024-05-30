c2dc9cbc37ad4d22e4e221d25826167523d37413
Pranav Kumar <96071514+PraNavKumAr01@users.noreply.github.com>
2024-05-30 19:33:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
 from graph import WorkFlow
@@ -29,6 +30,19 @@ class ChatHistoryRequest(BaseModel):
     conversation_id: str
     
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000/session",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 agent = WorkFlow().local_agent
 
